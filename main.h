e75129872e8478a87104a0da4f5a325e68d05322
useryab <yabsiratekalegn53@gmail.com>
2023-12-04 19:19:22 -0800

    percent_print & string_print have been pushed to main

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define HEADER_FILE
 
 /*
- * all libraries might not be needed 
+ * all libraries might not be needed
  * just included them just in case
 */
 
@@ -10,18 +10,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
+#define BUFF_SIZE 1000000
 /*
- * tried to add prototypes 
- * is not finished 
+ * tried to add prototypes
+ * is not finished
 */
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int charsFormats(const char *format, va_list args);
-int _percent(va_list list);
-int _char(va_list list);
-int _string(va_list list);
-int _integer(va_list list);
+int c_print(va_list args);
+int print_percent(void);
+int s_print(va_list arg_types);
+int _print_decimal(va_list args, char *buff);
+void int_to_str(int n, char *buff);
 
 #endif
